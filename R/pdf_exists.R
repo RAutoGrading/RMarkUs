6f6c8551de138a672b5657be61c105b7ca053153
Nathalie Moon <nathalie.moon@utoronto.ca>
2023-11-23 15:36:46 -0500

    Updated test_pfd_exists function

--- R/pdf_exists.R
+++ R/pdf_exists.R
@@ -5,15 +5,31 @@
 #' If none is provided, the function will just check that a pdf file exists
 #' @return A list of variables from specified environment
 #' @export
-test_pdf_present <- function(filename=NULL) {
+test_pdf_present <- function(filename=NULL,error_message=NULL) {
+
   if(is.null(filename)){
-    pdffiles <- list.files(pattern = "*.pdf")
+    filename <- "*.pdf" # In this case we'll just check for any pdf file
   }
-  else{
-    pdffiles <- list.files(pattern = paste(filename, ".pdf", sep=""))
+  else if(!any(grepl(pattern=".pdf", x = filename))){
+    # In this case, the filename doesn't have a .pdf extension so we'll add it here
+    filename <- paste(filename, ".pdf", sep="")
   }
 
-  test_that("PDF present:", {
-    expect_true(!identical(pdffiles, character(0)))
-  })
+  pdffiles_in_folder <- list.files(pattern = "*.pdf")
+  test_name <- paste(filename, ".pdf file exists", sep="")
+
+  if (is.null(error_message)) {
+    error_message <- paste("Missing", filename, "file", sep=" ")
+  }
+  success_message <- paste(filename, "file is present")
+
+  tryCatch (
+    {
+      test_that(test_name, {expect_true(!identical(pdffiles, character(0)))})
+      print(success_message)
+    },
+    error = function(e) {
+      message(error_message)
+    }
+  )
 }# end test_pdf_present function

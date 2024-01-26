3557e20276d3981bd897f406cbebb496eef5d244
Nathalie Moon <nathalie.moon@utoronto.ca>
2024-01-25 21:01:13 -0500

    Initial commit of test_environment_loaded.R and incremented version number

---
+++ R/test_environment_loaded.R
@@ -0,0 +1,33 @@
+library(testthat)
+library(cli)
+library(utils)
+library(knitr)
+
+#' Test to check that environment loaded properly
+#' @param environment Name of the R object containing a loaded R session.
+#' This should be a list with one element for each variable in the loaded environment
+#' @param error_message A function that will generate the appropriate error message as a string for if the environment is  NULL
+#' Default is NULL and will use preset error message.
+#' @return Error message if one exists, otherwise will print that test has passed
+#' @export
+#'
+test_environment_loaded <- function(environment, error_message=NULL){
+
+  test_name <- paste(deparse(substitute(environment)), "loaded")
+
+  if (is.null(error_message)) {
+    error_message <- paste(environment,
+                           "is not loaded properly and is equal to NULL. Please reach out to your course instructor because this test should always pass.")
+  }
+  success_message <- paste(deparse(substitute(environment)), "is loaded properly and has length", length(environment))
+
+  tryCatch (
+    {
+      test_that(test_name, {expect_equal(!is.null(environment), TRUE)})
+      print(success_message)
+    },
+    error = function(e) {
+      message(error_message)
+    }
+  )# end tryCatch
+  }# end test_environment_loaded function

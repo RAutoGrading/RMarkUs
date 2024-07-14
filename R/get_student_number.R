b5baf830dbd61637f81f7f26449bb0b54fc48942
Siqi Zheng <timothyzheng2000@gmail.com>
2024-07-14 08:30:32 -0400

    Update get_student_number

--- R/get_student_number.R
+++ R/get_student_number.R
@@ -9,7 +9,6 @@ source("R/is_running_on_MarkUs.R")
 #' @import testthat
 #' @import cli
 #' @param student_number Student number provided by students
-#' @param solution_filename_keyword The file name of solution should contain 'sol' so that it can be used to identify whether the instructor is running the script
 #' @param filename_student_info The .csv file contains student information with 6 columns ("username", "LastName", "FirstName", "Tutorial", "StudentNumber", "email")
 #' @param MarkUs_environment_variable The name of the environmental variable on MarkUs
 #'
@@ -17,26 +16,51 @@ source("R/is_running_on_MarkUs.R")
 #' @export
 #'
 
+# get_student_number <- function(student_number = 1,
+#                                solution_filename_keyword = "sol",
+#                                filename_student_info = "student_info.csv",
+#                                MarkUs_environment_variable = "MARKUS_GROUP"){ 
+# # Call is_running_on_MarkUs(), false if not on MarkUs i.e., on a local computer
+# # return student number provided by the user/student  
+#   if(isFALSE(is_running_on_MarkUs(MarkUs_environment_variable))){ 
+#     return(student_number) 
+#   } 
+#   else{ 
+#     # We know we’re running on MarkUs 
+#     # Extract filename of current script, using 
+#     # basename(rstudioapi::getSourceEditorContext()$path) 
+#     # and saving string in filename_current_script 
+#     filename_current_script <- basename(rstudioapi::getSourceEditorContext()$path)
+#     
+#     # If it is an instructor solution, 'sol' should be contained in the file name
+#     if(isTRUE(grepl(solution_filename_keyword, filename_current_script))){
+#       
+#       # Load .csv that contains all student info
+#       student_mapping <- read.csv(filename_student_info)
+#       # rename the columns of the dataframe
+#       names(student_mapping) <- c("username", "LastName", "FirstName", 
+#                                   "Tutorial", "StudentNumber", "email")
+#       # Get current student number by finding the student ID in the system 
+#       MARKUS_GROUP <- Sys.getenv(c("MARKUS_GROUP"))
+#       # Match the student ID with the info in .csv to obtain student number
+#       student_number <- student_mapping[student_mapping$username == MARKUS_GROUP,]$StudentNumber
+#     }
+#   } 
+#   return(student_number) 
+# } 
+
 get_student_number <- function(student_number = 1,
                                solution_filename_keyword = "sol",
                                filename_student_info = "student_info.csv",
                                MarkUs_environment_variable = "MARKUS_GROUP"){ 
-# Call is_running_on_MarkUs(), false if not on MarkUs i.e., on a local computer
-# return student number provided by the user/student  
+  # Call is_running_on_MarkUs(), false if not on MarkUs i.e., on a local computer
+  # return student number provided by the user/student  
   if(isFALSE(is_running_on_MarkUs(MarkUs_environment_variable))){ 
     return(student_number) 
   } 
   else{ 
     # We know we’re running on MarkUs 
-    # Extract filename of current script, using 
-    # basename(rstudioapi::getSourceEditorContext()$path) 
-    # and saving string in filename_current_script 
-    filename_current_script <- basename(rstudioapi::getSourceEditorContext()$path)
-    
-    # If it is an instructor solution, 'sol' should be contained in the file name
-    if(isTRUE(grepl(solution_filename_keyword, filename_current_script))){
-      
-      # Load .csv that contains all student info
+    # Load .csv that contains all student info
       student_mapping <- read.csv(filename_student_info)
       # rename the columns of the dataframe
       names(student_mapping) <- c("username", "LastName", "FirstName", 
@@ -45,14 +69,11 @@ get_student_number <- function(student_number = 1,
       MARKUS_GROUP <- Sys.getenv(c("MARKUS_GROUP"))
       # Match the student ID with the info in .csv to obtain student number
       student_number <- student_mapping[student_mapping$username == MARKUS_GROUP,]$StudentNumber
-    }
-  } 
+  }
   return(student_number) 
 } 
 
-
-
-# Implementation of the above in the problemset.Rmd file 
+# Implementation of the above in the problemset-solution.Rmd file 
 
 # STUDENT_NUMBER <- get_student_number(student_number = 1) 
 

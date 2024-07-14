b6c461d15ee2443514007b5096bb5dc9dffca2c7
Siqi Zheng <timothyzheng2000@gmail.com>
2024-07-14 08:54:57 -0400

    Update get_student_number again

--- R/get_student_number.R
+++ R/get_student_number.R
@@ -11,10 +11,32 @@ source("R/is_running_on_MarkUs.R")
 #' @param student_number Student number provided by students
 #' @param filename_student_info The .csv file contains student information with 6 columns ("username", "LastName", "FirstName", "Tutorial", "StudentNumber", "email")
 #' @param MarkUs_environment_variable The name of the environmental variable on MarkUs
-#'
 #' @return The student number based on who and where the tests are run. If a student runs locally, the user-defined student number will be used. When tests are run on MarkUs, then the student number will be extracted from the environmental variable. 
 #' @export
-#'
+get_student_number <- function(student_number = 1,
+                               solution_filename_keyword = "sol",
+                               filename_student_info = "student_info.csv",
+                               MarkUs_environment_variable = "MARKUS_GROUP"){ 
+  # Call is_running_on_MarkUs(), false if not on MarkUs i.e., on a local computer
+  # return student number provided by the user/student  
+  if(isFALSE(is_running_on_MarkUs(MarkUs_environment_variable))){ 
+    return(student_number) 
+  } 
+  else{ 
+    # We know we’re running on MarkUs 
+    # Load .csv that contains all student info
+    student_mapping <- read.csv(filename_student_info)
+    # rename the columns of the dataframe
+    names(student_mapping) <- c("username", "LastName", "FirstName", 
+                                "Tutorial", "StudentNumber", "email")
+    # Get current student number by finding the student ID in the system 
+    MARKUS_GROUP <- Sys.getenv(c("MARKUS_GROUP"))
+    # Match the student ID with the info in .csv to obtain student number
+    student_number <- student_mapping[student_mapping$username == MARKUS_GROUP,]$StudentNumber
+  }
+  return(student_number) 
+} 
+
 
 # get_student_number <- function(student_number = 1,
 #                                solution_filename_keyword = "sol",
@@ -49,29 +71,6 @@ source("R/is_running_on_MarkUs.R")
 #   return(student_number) 
 # } 
 
-get_student_number <- function(student_number = 1,
-                               solution_filename_keyword = "sol",
-                               filename_student_info = "student_info.csv",
-                               MarkUs_environment_variable = "MARKUS_GROUP"){ 
-  # Call is_running_on_MarkUs(), false if not on MarkUs i.e., on a local computer
-  # return student number provided by the user/student  
-  if(isFALSE(is_running_on_MarkUs(MarkUs_environment_variable))){ 
-    return(student_number) 
-  } 
-  else{ 
-    # We know we’re running on MarkUs 
-    # Load .csv that contains all student info
-      student_mapping <- read.csv(filename_student_info)
-      # rename the columns of the dataframe
-      names(student_mapping) <- c("username", "LastName", "FirstName", 
-                                  "Tutorial", "StudentNumber", "email")
-      # Get current student number by finding the student ID in the system 
-      MARKUS_GROUP <- Sys.getenv(c("MARKUS_GROUP"))
-      # Match the student ID with the info in .csv to obtain student number
-      student_number <- student_mapping[student_mapping$username == MARKUS_GROUP,]$StudentNumber
-  }
-  return(student_number) 
-} 
 
 # Implementation of the above in the problemset-solution.Rmd file 
 

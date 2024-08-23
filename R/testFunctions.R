5371a6fcbf37a123ae3697e0bc1ded6906191ceb
Siqi Zheng <timothyzheng2000@gmail.com>
2024-08-23 15:42:29 -0400

    Remove all raw functions

--- R/testFunctions.R
+++ R/testFunctions.R
@@ -42,17 +42,8 @@ testScalar <- function(variableName,
   # This is cleaner and fixes issue of partial matching of variable names
   actualSoln <- instructor_environment[[variableName]]
   variables <- names(student_environment) # this should be a list of the names
-  # if(variableName %in% variables){
-  #   studentSoln <- student_environment[[variableName]]
-  # }
-  # else{
-  #   studentSoln <- NULL
-  # }
   studentSoln <- ifelse(variableName %in% variables, student_environment[[variableName]], NULL)
   datatype <- ifelse(is.null(datatype), class(instructor_environment[[variableName]]), datatype)
-  # if(is.null(datatype)){
-  #   datatype <- class(instructor_environment[[variableName]])
-  # }
 
   # Validating inputs
   if (length(actualSoln) != 1){
@@ -60,25 +51,9 @@ testScalar <- function(variableName,
   }
 
   if (isTRUE(check_datatype)){
-    # if (is.null(datatype)){
-    #   stop("The testScalar function requires a value for datatype when check_datatype=TRUE.")
-    # }
-
     if(isFALSE(datatype %in% c("numeric", "integer", "complex", "logical", "character"))){
       stop(paste("Invalid data type:", datatype, "- Only numeric, integer, complex, character or logical values are allowed. Please verify the variable type in the instructor solution via class() function."))
-    }
-    # if (datatype == "numeric" & !is.numeric(actualSoln)){
-    #   stop("In testScalar you indicated the datatype was 'numeric' but actualSoln is not a numeric; the datatype of actualSoln should agree with the value passed for datatype")
-    # }
-    # else if (datatype == "logical" & !is.logical(actualSoln)){
-    #   stop("In testScalar you indicated the datatype was 'logical' but actualSoln is not a logical; the datatype of actualSoln should agree with the value passed for datatype")
-    # }
-    # else if (datatype == "character" & !is.character(actualSoln)){
-    #   stop("In testScalar you indicated the datatype was 'character' but actualSoln is not a character object; the datatype of actualSoln should agree with the value passed for datatype")
-    # }
-    # else if (datatype == "double" & !is.double(actualSoln)){
-    #   stop("In testScalar you indicated the datatype was 'double' but actualSoln is not a double object; the datatype of actualSoln should agree with the value passed for datatype")
-    # }    
+    } 
     if(isFALSE(isa(actualSoln, datatype))){
       stop(paste("The data type of the variable in the instructor solution:", class(actualSoln), "\ndoes NOT match the data type specified in the function argument:", datatype, "\nYou may consider removing the datatype argument in the function call as the data type can be automatically defined based on the instructor solution."))
     }
@@ -104,22 +79,13 @@ testScalar <- function(variableName,
   }
 
   if (isTRUE(check_datatype)) {
-    # if (datatype == "numeric"){
-    #   # If student solution is integer, convert to double to facilitate comparison
-    #   if(is.integer(studentSoln)){
-    #     studentSoln <- as.double(studentSoln)
-    #   }
-    #   # In this case, we'll call dataTypeTest with 'double' because
-    #   datatype <- "double"
-    # }
-
     dataTypeTest(variableName, variables, studentSoln, datatype, error_message=data_error_msg)
   }
 
   if (isTRUE(check_correct)) {
     correctSolnTest(variableName, variables, studentSoln, actualSoln, type="scalar", error_message=correct_error_msg)
   }
-}# end testScalar_raw function
+}# end testScalar function
 
 
 
@@ -171,107 +137,31 @@ testVector <- function(variableName,
   # Extract information from inputs
   actualSoln <- instructor_environment[[variableName]]
   variables <- names(student_environment) # this should be a list of the names
-  if(variableName %in% variables){
-    studentSoln <- student_environment[[variableName]]
-  }
-  else{
-    studentSoln <- NULL
-  }
-  if(is.null(datatype)){
-    datatype <- class(instructor_environment[[variableName]])
-  }
-
-  testVector_raw(variableName = variableName,
-                 variables = variables,
-                 studentSoln = studentSoln,
-                 actualSoln = actualSoln,
-                 check_present = check_present, present_error_msg=present_error_msg,
-                 check_correct = check_correct, correct_error_msg=correct_error_msg,
-                 check_datatype = check_datatype, data_error_msg=data_error_msg,
-                 check_length = check_length, size_error_msg=NULL,
-                 type=type,
-                 order=order,
-                 datatype=datatype,
-                 simplify_datatype=simplify_datatype)
-
-
-
-}# end testVector
-
-#' Completes the tests for a vector, where the user can directly input values to be compared
-#' @param variableName The name of the variable in question
-#' @param variables Vector listing the names of variables in the student's environment
-#' @param studentSoln The student's answer, loaded from their submission
-#' @param actualSoln The actual answer, loaded from the solution file
-#' @param check_correct Boolean indicating whether a test will check if the solution is correct.
-#' Default is TRUE.
-#' @param correct_error_msg A function that will generate the appropriate error message as a string if the value is incorrect.
-#' Default is NULL and will use preset error message.
-#' @param check_present Boolean indicating whether a test will check if the variable exists.
-#' Default is TRUE.
-#' @param present_error_msg A function that will generate the appropriate error message as a string for if variable does not exist.
-#' Default is NULL and will use preset error message.
-#' @param check_length Boolean indicating whether a test will check if the length of the vector is correct.
-#' Default is TRUE.
-#' @param size_error_msg A function that will generate the appropriate error message as a string if the size is incorrect.
-#' Default is NULL and will use preset error message.
-#' @param check_datatype Boolean indicating whether a test will check if the data type is correct.
-#' Default is FALSE.
-#' @param datatype Optional argument (string) for data type expected for solution if check_datatype is TRUE.
-#' Options are `numeric`, `character`, `logical`, and `double` (if numeric, then tests pass if solution is either double or integer)
-#' @param data_error_msg A function that will generate the appropriate error message as a string for if the data type is not correct.
-#' Default is NULL and will use preset error message.
-#' @param type Optional argument (string) for data type expected for solution if check_datatype is TRUE.
-#' DEFAULT is vector. Options are `vector`, `scalar`, `list`
-#' @param order Boolean indicating if order matters in correctness.
-#' Default is TRUE.
-#' @param simplify_datatype Boolean indicating whether datatype tests will pass if a student passes a tibble when the solutions expect a primitive instead. Defaults to FALSE
-#' @return Error message if one exists, otherwise will print that every test has passed.
-#' @export
-testVector_raw <- function(variableName, variables, studentSoln, actualSoln,
-                           check_correct=TRUE, correct_error_msg=NULL,
-                           check_present=TRUE, present_error_msg=NULL,
-                           check_length=TRUE, size_error_msg=NULL,
-                           check_datatype=FALSE, data_error_msg=NULL,
-                           datatype=NULL,
-                           type='vector',
-                           order=TRUE,
-                           simplify_datatype=FALSE) {
+  studentSoln <- ifelse(variableName %in% variables, student_environment[[variableName]], NULL)
+  datatype <- ifelse(is.null(datatype), class(instructor_environment[[variableName]]), datatype)
 
   # Validating inputs
   if (isTRUE(check_datatype)){
-    if (is.null(datatype)){
-      stop("The testVector function requires a value for datatype when check_datatype=TRUE.")
+    if(isFALSE(datatype %in% c("numeric", "integer", "complex", "logical", "character"))){
+      stop(paste("Invalid data type:", datatype, "- Only numeric, integer, complex, character or logical values are allowed. Please verify the variable type in the instructor solution via class() function."))
     }
-
-    if(isFALSE(datatype %in% c("numeric", "character", "logical", "integer", "complex"))){
-      stop("The testVector function received an invalid value for the datatype argument; valid values are numeric, character, and logical")
-    }
-
     if(isFALSE(isa(actualSoln, datatype))){
-      stop(paste("The class of your answer is", class(actualSoln), "but the correct class should be", datatype))
+      stop(paste("The data type of the variable in the instructor solution:", class(actualSoln), "\ndoes NOT match the data type specified in the function argument:", datatype, "\nYou may consider removing the datatype argument in the function call as the data type can be automatically defined based on the instructor solution."))
     }
-  }
-
-  if (type == "vector" & !is.vector(actualSoln)){
-    stop("In testVector, you passed type='vector' but actualSoln is not a vector")
-  }
-  if (type == "scalar" & length(actualSoln) > 1){
-    stop("In testVector, you passed type=scalar but actualSoln has length greater than 1.")
   }
 
 
   #simplify_student_answer <- TRUE # This could be turned into a parameter to turn on or off
-  if(isTRUE(simplify_datatype)){
-    # Try to convert the student's answer to a simpler datatype for comparison (e.g. tibble to single number)
-    if(is.data.frame(studentSoln)){
-      warning(paste("You submitted a dataframe when an object with datatype", datatype, "was expected"))
-      if(ncol(studentSoln)==1 | nrow(studentSoln)==1){
-        studentSoln <- studentSoln |> as.vector() |> unlist()
-        datatype <- class(studentSoln)
-      }
-    }
-  }
+  # if(isTRUE(simplify_datatype)){
+  #   # Try to convert the student's answer to a simpler datatype for comparison (e.g. tibble to single number)
+  #   if(is.data.frame(studentSoln)){
+  #     warning(paste("You submitted a dataframe when an object with datatype", datatype, "was expected"))
+  #     if(ncol(studentSoln)==1 | nrow(studentSoln)==1){
+  #       studentSoln <- studentSoln |> as.vector() |> unlist()
+  #       datatype <- class(studentSoln)
+  #     }
+  #   }
+  # }
 
 
   # Unit tests
@@ -280,15 +170,6 @@ testVector_raw <- function(variableName, variables, studentSoln, actualSoln,
   }
 
   if (isTRUE(check_datatype)) {
-    if (datatype == "numeric"){
-      # If student solution is integer, convert to double to facilitate comparison
-      if(is.integer(studentSoln)){
-        studentSoln <- as.double(studentSoln)
-      }
-      # In this case, we'll call dataTypeTest with 'double' because
-      datatype <- "double"
-    }
-
     dataTypeTest(variableName, variables, studentSoln, datatype, error_message=data_error_msg)
   }
 
@@ -299,8 +180,7 @@ testVector_raw <- function(variableName, variables, studentSoln, actualSoln,
   if (isTRUE(check_correct)) {
     correctSolnTest(variableName, variables, studentSoln, actualSoln, type=type, order, error_message=correct_error_msg)
   }
-
-}# end testVector_raw
+}# end testVector
 
 
 #' DataFrame Tests
@@ -348,65 +228,7 @@ testDataFrame <- function(variableName,
   # Extract information from inputs
   actualSoln <- instructor_environment[[variableName]]
   variables <- names(student_environment) # this should be a list of the names
-  if(variableName %in% variables){
-    studentSoln <- student_environment[[variableName]]
-  }
-  else{
-    studentSoln <- NULL
-  }
-
-  testDataFrame_raw(variableName = variableName,
-                    variables = variables,
-                    studentSoln = studentSoln,
-                    actualSoln = actualSoln,
-                    check_present = check_present, present_error_msg=present_error_msg,
-                    check_correct = check_correct, correct_error_msg=correct_error_msg,
-                    check_attributes = check_attributes, attributes_error_msg=attributes_error_msg,
-                    check_size = check_size, size_error_msg=size_error_msg,
-                    col_order=col_order)
-
-}# end testDataFrame function
-
-
-#' Completes all of the tests for a DataFrame, where the user can directly input values to be compared
-#' @param variableName The name of the variable in question
-#' @param variables Vector listing the names of variables in the student's environment
-#' @param studentSoln The student's answer, loaded from their submission
-#' @param actualSoln The actual answer, loaded from the solution file
-#' @param check_correct Boolean indicating whether a test will check if the solution is correct.
-#' Default is TRUE.
-#' @param correct_error_msg A function that will generate the appropriate error message as a string if the value is incorrect.
-#' Default is NULL and will use preset error message.
-#' @param check_present Boolean indicating whether a test will check if the variable exists.
-#' Default is TRUE.
-#' @param present_error_msg A function that will generate the appropriate error message as a string for if variable does not exist.
-#' Default is NULL and will use preset error message.
-#' @param check_size Boolean indicating whether a test will check if the size of the dataframe is correct.
-#' Default is TRUE.
-#' @param size_error_msg A function that will generate the appropriate error message as a string if the size is incorrect.
-#' Default is NULL and will use preset error message.
-#' @param check_attributes Boolean indicating whether a test will check if the attributes of the dataframe are correct.
-#' Default is TRUE.
-#' @param attributes_error_msg A function that will generate the appropriate error message as a string if the attributes of the dataframe are incorrect.
-#' Default is NULL and will use preset error message.
-#' @param check_class Boolean indicating whether a test will check if the variable classes of the dataframe are correct.
-#' Default is TRUE.
-#' @param class_error_msg A function that will generate the appropriate error message as a string if the classes of the dataframe are incorrect.
-#' Default is NULL and will use preset error message.
-#' @param col_order Boolean indicating if order matters in correctness.
-#' Default is FALSE
-#' @return Error message if one exists, otherwise will print that every test has passed.
-#' @export
-testDataFrame_raw <- function(variableName,
-                              variables,
-                              studentSoln,
-                              actualSoln,
-                              check_correct=TRUE, correct_error_msg=NULL,
-                              check_present=TRUE, present_error_msg=NULL,
-                              check_size=TRUE, size_error_msg=NULL,
-                              check_attributes=FALSE, attributes_error_msg=NULL,
-                              check_class=TRUE, class_error_msg=NULL,
-                              col_order=TRUE) {
+  studentSoln <- ifelse(variableName %in% variables, student_environment[[variableName]], NULL)
 
   # Validating inputs
 
@@ -431,7 +253,7 @@ testDataFrame_raw <- function(variableName,
     correctSolnTest(variableName, variables, studentSoln, actualSoln, type="dataframe", order=col_order, error_message=correct_error_msg)
   }
 
-}# end testDataFrame_raw function
+}# end testDataFrame function
 
 
 
